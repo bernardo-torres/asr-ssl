4130c43f78a2571f21661bdb62eed87e336c1f46
torresbf <beftorres@hotmail.com>
2022-04-01 18:00:04 +0200

    Changing model factory

--- src/model.py
+++ src/model.py
@@ -1,15 +1,21 @@
 
 from transformers import Wav2Vec2ForCTC, HubertForCTC, WavLMForCTC
 
-def model_factory(processor,  
+def model_factory(model_id,
+                 cache_dir,
+                 activation_dropout,
+                 attention_dropout,
+                 hidden_dropout,
+                 feat_proj_dropout,
+                 mask_time_prob,
+                 gradient_checkpointing,
+                 layerdrop,
+                 ctc_loss_reduction,
+                 pad_token_id,
+                 vocab_size,
                   model_type='wav2vec',
-                  model_id=None,
                   final_layer=None,
-                  attention_dropout=0.1,
-                  hidden_dropout=0.1,
-                  feat_proj_dropout=0.0,
-                  mask_time_prob=0.05,
-                  layerdrop=0.1,):
+                  freeze_encoder=False):
 
     """
     processor: wav2vec processor with vocab configurations
@@ -27,44 +33,53 @@ def model_factory(processor,
         if model_id==None:
             model_id = "facebook/wav2vec2-base"
         model = Wav2Vec2ForCTC.from_pretrained(
-        model_id, 
-        attention_dropout=attention_dropout,
-        hidden_dropout=hidden_dropout,
-        feat_proj_dropout=feat_proj_dropout,
-        mask_time_prob=mask_time_prob,
-        layerdrop=layerdrop,
-        ctc_loss_reduction="mean", 
-        pad_token_id=processor.tokenizer.pad_token_id,
-        vocab_size=len(processor.tokenizer)
+                        model_id, 
+                        cache_dir=cache_dir,
+                        activation_dropout=activation_dropout,
+                        attention_dropout=attention_dropout,
+                        hidden_dropout=hidden_dropout,
+                        feat_proj_dropout=feat_proj_dropout,
+                        mask_time_prob=mask_time_prob,
+                        gradient_checkpointing=gradient_checkpointing,
+                        layerdrop=layerdrop,
+                        ctc_loss_reduction=ctc_loss_reduction,
+                        pad_token_id=pad_token_id,
+                        vocab_size=vocab_size
         )
     elif model_type == 'hubert':
         if model_id==None:
             model_id = "facebook/hubert-large-ls960-ft"
         model = HubertForCTC.from_pretrained(
-        model_id, 
-        attention_dropout=attention_dropout,
-        hidden_dropout=hidden_dropout,
-        feat_proj_dropout=feat_proj_dropout,
-        mask_time_prob=mask_time_prob,
-        layerdrop=layerdrop,
-        ctc_loss_reduction="mean", 
-        pad_token_id=processor.tokenizer.pad_token_id,
-        vocab_size=len(processor.tokenizer)
-        )
+                    model_id, 
+                    cache_dir=cache_dir,
+                    activation_dropout=activation_dropout,
+                    attention_dropout=attention_dropout,
+                    hidden_dropout=hidden_dropout,
+                    feat_proj_dropout=feat_proj_dropout,
+                    mask_time_prob=mask_time_prob,
+                    gradient_checkpointing=gradient_checkpointing,
+                    layerdrop=layerdrop,
+                    ctc_loss_reduction=ctc_loss_reduction,
+                    pad_token_id=pad_token_id,
+                    vocab_size=vocab_size)
+        
     elif model_type == 'wavlm':
         if model_id == None:
             model_id = "patrickvonplaten/wavlm-libri-clean-100h-base-plus"
         model = WavLMForCTC.from_pretrained(
-        model_id, 
-        attention_dropout=attention_dropout,
-        hidden_dropout=hidden_dropout,
-        feat_proj_dropout=feat_proj_dropout,
-        mask_time_prob=mask_time_prob,
-        layerdrop=layerdrop,
-        ctc_loss_reduction="mean", 
-        pad_token_id=processor.tokenizer.pad_token_id,
-        vocab_size=len(processor.tokenizer)
-        )
+                    model_id, 
+                    cache_dir=cache_dir,
+                    activation_dropout=activation_dropout,
+                    attention_dropout=attention_dropout,
+                    hidden_dropout=hidden_dropout,
+                    feat_proj_dropout=feat_proj_dropout,
+                    mask_time_prob=mask_time_prob,
+                    gradient_checkpointing=gradient_checkpointing,
+                    layerdrop=layerdrop,
+                    ctc_loss_reduction=ctc_loss_reduction,
+                    pad_token_id=pad_token_id,
+                    vocab_size=vocab_size)
+                    
     
     if final_layer is not None:
         # For CTC loss the final layer default is nn.Linear(output_hidden_size, config.vocab_size)
@@ -72,9 +87,10 @@ def model_factory(processor,
         model.post_init()
         
     # set requires_grad to False for the feature extraction part (suff. trained during pre-training)
-    model.freeze_feature_encoder()
+    if freeze_encoder:
+        model.freeze_feature_encoder()
 
     # gradient checkpointing to save memory
-    model.gradient_checkpointing_enable()
+    #model.gradient_checkpointing_enable()
 
     return model
